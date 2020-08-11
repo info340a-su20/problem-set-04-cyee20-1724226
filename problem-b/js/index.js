b01fdadcf8625f70c12f230017f7e90baaeea5f3
cyee20-1724226 <cyee20@uw.edu>
2020-08-10 19:10:25 -0700

    Completed Set 4

--- problem-b/js/index.js
+++ problem-b/js/index.js
@@ -1,18 +1,21 @@
-'use strict';
+"use strict";
 
 /* Define a function `addFour()` that takes a single argument 
    and returns a value 4 greater than the input.*/
+function addFour(num) {
+  return num + 4;
+}
 
-   
 /* Create and log a variable `twelve` that is the result of passing 8 to your
    addFour() function. */
+let twelve = addFour(8);
+console.log(twelve);
 
-   
 /* Create and log a variable `twelveString` that is the result of passing "8" 
    (a string) to your addFour() function. Consider what this tells you about how
   the function should be explained (e.g., in a comment). */
-
-  
+let twelveString = addFour("8");
+console.log(twelveString);
 
 /* Define a function `compoundInterest()` that takes three parameters: 
      1. an initial bank balance (principle, in dollars)
@@ -25,8 +28,9 @@
    You can call the method and log the result to check your work. Compare to
      http://www.mathwarehouse.com/calculators/continuous-compound-interest-calculator.php
 */
-
-
+function compoundInterest(balance, rate, years) {
+  return Math.exp(rate * years) * balance;
+}
 
 /* Define a function `fizzBuzz()` that takes in a single number as an argument.
    The function should *return* an _array_ of numbers from 1 to the argument. 
@@ -36,7 +40,21 @@
    should contain "FizzBuzz" instead of the number.
    The returned array should be empty for arguments less than 1. */
 
-   
+function fizzBuzz(num) {
+  let array = [];
+  for (let i = 1; i <= num; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      array.push("FizzBuzz");
+    } else if (i % 5 == 0) {
+      array.push("Buzz");
+    } else if (i % 3 == 0) {
+      array.push("Fizz");
+    } else {
+      array.push(i);
+    }
+  }
+  return array;
+}
 
 /* Define a function `getLetterFrequencies()` that takes in a single string as 
    an argument. The function should *return* an Object whose keys are characters
@@ -47,8 +65,18 @@
    each letter, increase the value associated with that key by one. Watch out 
    for if the letter is not in the Object yet!
    You can test this method with a word like "Mississippi". */
-
-   
+function getLetterFrequencies(string) {
+  let freq = {};
+  for (let i = 0; i < string.length; i++) {
+    let char = string[i];
+    if (isNaN(freq[char])) {
+      freq[char] = 1;
+    } else {
+      freq[char]++;
+    }
+  }
+  return freq;
+}
 
 /* Create a variable `deck` that represents a deck of modern playing cards
    (https://en.wikipedia.org/wiki/Playing_card). This variable should be an
@@ -62,9 +90,19 @@
     
     You can log out the `deck` to check your work! */
 
-    
+let deck = [];
+let suit = ["hearts", "diamonds", "clubs", "spades"];
 
-//You can test the below functions by creating e.g., a `pokerHand` array that 
+for (let i = 0; i < 4; i++) {
+  for (let j = 2; j <= 14; j++) {
+    let card = {};
+    card.rank = j;
+    card.suit = suit[i];
+    deck.push(card);
+  }
+}
+
+//You can test the below functions by creating e.g., a `pokerHand` array that
 //contains five cards from the `deck`.
 
 /* Define a function `containsQueenOfHearts()` that takes in an array of "card"
@@ -72,19 +110,40 @@
    is in that array.
    Hint: use a loop to check each card. */
 
-   
+function containsQueenOfHearts(pokerHand) {
+  for (let i = 0; i < pokerHand.length; i++) {
+    if (pokerHand[i].rank == 12) {
+      return true;
+    }
+  }
+  return false;
+}
 
 /* Define a function `getHighCard()` that takes in an array of "card" objects
   and returns the card object with the highest value. The "high card" is the one
   with the highest rank. Cards of different suits but the same rank are 
   considered to have the same value, and either is a valid result */
-
-  
+function getHighCard(pokerHand) {
+  let highCard = pokerHand[0];
+  for (let i = 1; i < pokerHand.length; i++) {
+    if (highCard.rank < pokerHand[i].rank) {
+      highCard = pokerHand[i];
+    }
+  }
+  return highCard;
+}
 
 /* Define a function `isFlush()` that takes in an array of "card" objects and
    returns whether or not the cards all have the same _suit_. */
-
-   
+function isFlush(pokerHand) {
+  let card = pokerHand[0].suit;
+  for (let i = 1; i < pokerHand.length; i++) {
+    if (card != pokerHand[i].suit) {
+      return false;
+    }
+  }
+  return true;
+}
 
 /* Extra challenge: define a function `hasPair()` that takes in an array of 
    "card" objects and returns whether or not there is at least one _pair_ (two 
@@ -92,27 +151,21 @@
    Double challenge: return the rank of the pair of cards with the highest rank 
    (e.g., if the hand contains more than one pair!) */
 
-
-
 //Make functions and variables available to tester. DO NOT MODIFY THIS.
-if(typeof module !== 'undefined' && module.exports){
+if (typeof module !== "undefined" && module.exports) {
   /* eslint-disable */
-  if(typeof addFour !== 'undefined') 
-    module.exports.addFour = addFour;
-  if(typeof twelveString !== 'undefined') 
+  if (typeof addFour !== "undefined") module.exports.addFour = addFour;
+  if (typeof twelveString !== "undefined")
     module.exports.twelveString = twelveString;
-  if(typeof compoundInterest !== 'undefined') 
+  if (typeof compoundInterest !== "undefined")
     module.exports.compoundInterest = compoundInterest;
-  if(typeof fizzBuzz !== 'undefined') 
-    module.exports.fizzBuzz = fizzBuzz;
-  if(typeof getLetterFrequencies !== 'undefined')
+  if (typeof fizzBuzz !== "undefined") module.exports.fizzBuzz = fizzBuzz;
+  if (typeof getLetterFrequencies !== "undefined")
     module.exports.getLetterFrequencies = getLetterFrequencies;
-  if(typeof deck !== 'undefined')
-    module.exports.deck = deck;
-  if(typeof containsQueenOfHearts !== 'undefined')
+  if (typeof deck !== "undefined") module.exports.deck = deck;
+  if (typeof containsQueenOfHearts !== "undefined")
     module.exports.containsQueenOfHearts = containsQueenOfHearts;
-  if(typeof getHighCard !== 'undefined')
+  if (typeof getHighCard !== "undefined")
     module.exports.getHighCard = getHighCard;
-  if(typeof isFlush !== 'undefined')
-    module.exports.isFlush = isFlush;
+  if (typeof isFlush !== "undefined") module.exports.isFlush = isFlush;
 }
